1d6b3f664c4134d3372c4105f3cb9e792f184dff
Carzita <Carzita@users.noreply.github.com>
2020-10-15 17:57:50 +0200

    overwritten the p2p file with the new one, see branch take2 for the old file if code need to be extracted

--- src/chatapplication_server/components/ClientSocketEngine/P2PClient.java
+++ src/chatapplication_server/components/ClientSocketEngine/P2PClient.java
@@ -26,14 +26,8 @@ import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 import java.net.*;
-import java.security.*;
-import java.security.spec.InvalidKeySpecException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import static chatapplication_server.components.GFG.findPrimitive;
-import static chatapplication_server.components.GFG.power;
-import static java.math.BigInteger.probablePrime;
 
 /**
  *
@@ -50,16 +44,20 @@ public class P2PClient extends JFrame implements ActionListener
     private final JTextField tf;
     private final JTextArea ta;
     protected boolean keepGoing;
-    JButton send, start;
+    JButton Send, stopStart;
+    JButton connectStop;
 
-    SecureRandom rnd = new SecureRandom();
-    private double secretKey;
-    int p;
-    int g;
-    int a;
-    int b;
-    int A;
-    int B;
+    /** Client Socket and output stream... */
+    Socket socket = null;
+    ObjectOutputStream sOutput;
+
+    private ListenFromClient clientServer;
+
+    /** Flag indicating whether the Socket Server is running at one of the Clients... */
+    boolean isRunning;
+
+    /** Flag indicating whether another client is connected to the Socket Server... */
+    boolean isConnected;
 
     P2PClient(){
         super("P2P Client Chat");
@@ -77,8 +75,8 @@ public class P2PClient extends JFrame implements ActionListener
 
         tfsPort=new JTextField(5);
         tfsPort.setHorizontalAlignment(SwingConstants.RIGHT);
-        start=new JButton("Start");
-        start.addActionListener(this);
+        stopStart=new JButton("Start");
+        stopStart.addActionListener(this);
 
         serverAndPort.add(new JLabel("Receiver's Port No:  "));
         serverAndPort.add(tfPort);
@@ -107,12 +105,16 @@ public class P2PClient extends JFrame implements ActionListener
 //        centerPanel.add(new JScrollPane(ta2));
         add(centerPanel, BorderLayout.CENTER);
 
+        connectStop = new JButton( "Connect" );
+        connectStop.addActionListener(this);
 
-        send = new JButton("Send");
-        send.addActionListener(this);
+        Send = new JButton("Send");
+        Send.addActionListener(this);
+        Send.setVisible( false );
         JPanel southPanel = new JPanel();
-        southPanel.add(send);
-        southPanel.add(start);
+        southPanel.add( connectStop );
+        southPanel.add(Send);
+        southPanel.add(stopStart);
         JLabel lbl=new JLabel("Sender's Port No:");
         southPanel.add(lbl);
         tfsPort.setText("0");
@@ -125,21 +127,57 @@ public class P2PClient extends JFrame implements ActionListener
         setSize(600, 600);
         setVisible(true);
         tf.requestFocus();
+
+        isRunning = false;
+        isConnected = false;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         Object o = e.getSource();
-        if(o == send){
-            if ( tfPort.getText().equals( ConfigManager.getInstance().getValue( "Server.PortNumber" ) ) )
+
+        if ( o == connectStop )
+        {
+            if ( connectStop.getText().equals( "Connect" ) && isConnected == false )
             {
-                display( "Cannot give the same port number as the Chat Application Server - Please give the port number of the peer client to communicate!\n" );
-                return;
+                if ( tfPort.getText().equals( ConfigManager.getInstance().getValue( "Server.PortNumber" ) ) )
+                {
+                    display( "Cannot give the same port number as the Chat Application Server - Please give the port number of the peer client to communicate!\n" );
+                    return;
+                }
+
+                /** Connect to the Socket Server instantiated by the other client... */
+                this.connect();
             }
-            this.send(tf.getText());
+            else if ( connectStop.getText().equals( "Disconnect" ) && isConnected == true )
+            {
+                this.disconnect();
+            }
         }
-        if(o == start){
-            new ListenFromClient().start();
+        else if ( o == Send )
+        {
+            /** Try to send the message to the other communicating party, if we have been connected... */
+            if ( isConnected == true )
+            {
+                this.send( tf.getText() );
+            }
+        }
+        else if(o == stopStart)
+        {
+            if ( stopStart.getText().equals( "Start" ) && isRunning == false)
+            {
+                clientServer = new ListenFromClient();
+                clientServer.start();
+                isRunning = true;
+                stopStart.setText( "Stop" );
+            }
+            else if ( stopStart.getText().equals( "Stop" ) && isRunning == true )
+            {
+                clientServer.shutDown();
+                clientServer.stop();
+                isRunning = false;
+                stopStart.setText( "Start" );
+            }
         }
     }
 
@@ -148,175 +186,186 @@ public class P2PClient extends JFrame implements ActionListener
         ta.setCaretPosition(ta.getText().length() - 1);
     }
 
-    private void beginDH() {
-        p = probablePrime(16, new SecureRandom()).intValue();
-        g = findPrimitive(p);
-
-        System.out.println("Alice p:" + p);
-        System.out.println("Alice g:" + g);
-        send("I:" + p + "," + g);
-
-        SecureRandom ranGen = new SecureRandom();
-        b = ranGen.nextInt(99999) & Integer.MAX_VALUE;
-        B = power(g, b, p); // calculation of A
-
-        System.out.println("Alice B:" + B);
-
-        send("B:" + B);
-    }
-
-
-
-    private void computeA(String msg) {
-        int i = msg.indexOf(",");
-        p = Integer.parseInt(msg.substring(0,i));
-        g = Integer.parseInt(msg.substring(i+1));
-        System.out.println("Bob p:" + p);
-        System.out.println("Bob g:" + g);
-
-        SecureRandom ranGen = new SecureRandom();
-        a = ranGen.nextInt(99999) & Integer.MAX_VALUE;
-
-        A = power(g, a, p); // calculation of A
-
-        System.out.println("Bob A: " + A);
-
-        send("A:" + A);
-    }
-
-    private void computeB() {
-        SecureRandom ranGen = new SecureRandom();
-        b = ranGen.nextInt(99999) & Integer.MAX_VALUE;
-        B = power(g, b, p); // calculation of A
-
-        System.out.println("Alice B:" + B);
-
-        send("B:" + B);
-    }
-
-    private double computeKeyFromB(String msg) {
-        B = Integer.parseInt(msg);
-        System.out.println("Bob B: " + B);
-        return power(B, a, p);
-    }
-
-    private double computeKeyFromA(String msg) {
-        A = Integer.parseInt(msg);
-        System.out.println("Alice A: " + A);
-        return power(A,b, p);
-    }
-
-
-    public boolean send(String str){
-        Socket socket;
-        ObjectOutputStream sOutput;		// to write on the socket
-        // try to connect to the server
+    /**
+     * Method that is invoked when a client wants to connect to the Socket Server spawn from another client in order to initiate their P2P communication.
+     *
+     * @return TRUE if the connection was successful; FALSE otherwise
+     */
+    public boolean connect()
+    {
+        /* Try to connect to the Socket Server... */
         try {
-            socket = new Socket(tfServer.getText(), Integer.parseInt(tfPort.getText()));
+            if (isConnected == false)
+            {
+                socket = new Socket(tfServer.getText(), Integer.parseInt(tfPort.getText()));
+
+                sOutput = new ObjectOutputStream(socket.getOutputStream());
+                isConnected = true;
+                Send.setVisible( true );
+                connectStop.setText( "Disconnect" );
+
+                return true;
+            }
+        }
+        catch (IOException eIO) {
+            display("The Socket Server from the other side has not been fired up!!\nException creating new Input/output Streams: " + eIO.getMessage() + "\n");
+            isConnected = false;
+            Send.setVisible( false );
+            connectStop.setText( "Connect" );
+            return false;
         }
         // if it failed not much I can so
         catch(Exception ec) {
-            display("Error connectiong to server:" + ec.getMessage() + "\n");
+            display("Error connecting to server:" + ec.getMessage() + "\n");
+            isConnected = false;
+            Send.setVisible( false );
+            connectStop.setText( "Connect" );
             return false;
-        }
-
-        /* Creating both Data Stream */
-        try
-        {
-//			sInput  = new ObjectInputStream(socket.getInputStream());
-            sOutput = new ObjectOutputStream(socket.getOutputStream());
-        }
-        catch (IOException eIO) {
-            display("Exception creating new Input/output Streams: " + eIO);
-            return false;
-        }
-
-        try {
-            sOutput.writeObject(new ChatMessage(str.length(), str));
-            display("You: " + str);
-            sOutput.close();
-            socket.close();
-            if (str.equals("Start DH")) {
-                beginDH();
-            }
-        } catch (IOException ex) {
-            display("Exception creating new Input/output Streams: " + ex);
         }
 
         return true;
     }
 
-    private class ListenFromClient extends Thread{
-        public ListenFromClient() {
-            keepGoing=true;
+    /**
+     * Method that is invoked when we want do disconnect from a Socket Server (spawn by another client); this, basically, reflects the stopping of a P2P communication
+     *
+     * @return TRUE if the disconnect was successful; FALSE, otherwise
+     */
+    public boolean disconnect()
+    {
+        /** Disconnect from the Socket Server that we are connected... */
+        try
+        {
+            if ( isConnected == true )
+            {
+                /** First, close the output stream... */
+                sOutput.close();
+
+                /** Then, close the socket... */
+                socket.close();
+
+                /** Re-initialize the parameters... */
+                isConnected = false;
+                Send.setVisible( false );
+                connectStop.setText( "Connect" );
+
+                return true;
+            }
+        }
+        catch( IOException ioe )
+        {
+            display( "Error closing the socket and output stream: " + ioe.getMessage() + "\n" );
+
+            /** Re-initialize the parameters... */
+            isConnected = false;
+            Send.setVisible( false );
+            connectStop.setText( "Connect" );
+            return false;
         }
 
-        @Override
-        public void run() {
+        return true;
+    }
+
+    public boolean send(String str)
+    {
+        try {
+            sOutput.writeObject(new ChatMessage(str.length(), str));
+            display("You: " + str);
+            //sOutput.close();
+            //socket.close();
+        } catch (IOException ex) {
+            display("The Client's Server Socket was closed!!\nException creating output stream: " + ex.getMessage());
+            this.disconnect();
+            return false;
+        }
+
+        return true;
+    }
+
+    private class ListenFromClient extends Thread
+    {
+        ServerSocket serverSocket;
+        Socket socket;
+        ObjectInputStream sInput = null;
+        boolean clientConnect;
+
+        public ListenFromClient()
+        {
             try
             {
                 // the socket used by the server
-                ServerSocket serverSocket = new ServerSocket(Integer.parseInt(tfsPort.getText()));
-                //display("Server is listening on port:"+tfsPort.getText());
+                serverSocket = new ServerSocket(Integer.parseInt(tfsPort.getText()));
                 ta.append("Server is listening on port:"+tfsPort.getText() + "\n");
                 ta.setCaretPosition(ta.getText().length() - 1);
+                clientConnect = false;
+                keepGoing = true;
+            }
+            catch ( IOException ioe )
+            {
+                System.out.println("[P2PClient]:: Error firing up Socket Server " + ioe.getMessage());
+            }
+        }
 
-                // infinite loop to wait for connections
-                while(keepGoing)
+        @Override
+        public void run()
+        {
+            // infinite loop to wait for messages
+            while(keepGoing)
+            {
+                /** Wait only when there are no connections... */
+                try
                 {
-                    // format message saying we are waiting
-
-                    Socket socket = serverSocket.accept();  	// accept connection
-
-                    ObjectInputStream sInput=null;		// to write on the socket
-
-                    /* Creating both Data Stream */
-                    try
+                    if ( !clientConnect )
                     {
+                        socket = serverSocket.accept();  	// accept connection
                         sInput = new ObjectInputStream(socket.getInputStream());
-                    }
-                    catch (IOException eIO) {
-                        display("Exception creating new Input/output Streams: " + eIO);
-                    }
-
-                    try {
-                        String msg = ((ChatMessage) sInput.readObject()).getMessage();
-                        System.out.println("Msg:"+msg);
-                        switch(msg.substring(0,2)) {
-                            case "I:":
-                                System.out.println("Client receives p and g");
-                                computeA(msg.substring(2));
-                                break;
-                            case "A:":
-                                System.out.println("Client receives A");
-//                                computeB();
-                                secretKey = computeKeyFromA(msg.substring(2));
-                                System.out.println("Alice secret key: " + secretKey);
-                                break;
-                            case "B:":
-                                System.out.println("Client receives A");
-                                secretKey = computeKeyFromB(msg.substring(2));
-                                System.out.println("Bob secret key:" + secretKey);
-                                break;
-                        }
-
-                        display(socket.getInetAddress()+": " + socket.getPort() + ": " + msg);
-                        sInput.close();
-                        socket.close();
-                    } catch (IOException ex) {
-                        display("Exception creating new Input/output Streams: " + ex);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(P2PClient.class.getName()).log(Level.SEVERE, null, ex);
+                        clientConnect = true;
                     }
                 }
+                catch (IOException ex)
+                {
+                    display("The Socket Server was closed: " + ex.getMessage());
+                }
+
+                // format message saying we are waiting
+                try {
+                    String msg = ((ChatMessage) sInput.readObject()).getMessage();
+                    System.out.println("Msg:"+msg);
+                    display(socket.getInetAddress()+": " + socket.getPort() + ": " + msg);
+                    //sInput.close();
+                    //socket.close();
+                }
+                catch (IOException ex)
+                {
+                    display("Could not ready correctly the messages from the connected client: " + ex.getMessage());
+                    clientConnect = false;
+                }
+                catch (ClassNotFoundException ex) {
+                    Logger.getLogger(P2PClient.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-            // something went bad
-            catch (IOException e) {
-//            String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
-//			display(msg);
+        }
+
+        public void shutDown()
+        {
+            try
+            {
+                keepGoing = false;
+                if ( socket != null )
+                {
+                    sInput.close();
+                    socket.close();
+                }
+
+                if (serverSocket != null)
+                {
+                    serverSocket.close();
+                }
+            }
+            catch ( IOException ioe )
+            {
+                System.out.println("[P2PClient]:: Error closing Socket Server " + ioe.getMessage());
             }
         }
     }
-
-
 }
